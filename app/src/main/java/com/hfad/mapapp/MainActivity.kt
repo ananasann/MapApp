6f73d7932d887e667e308e96d44dc5a7118e1563
Ananasann <paramonovaann39@gmail.com>
2021-10-29 17:35:27 +0300

    main activity errors fixed

--- app/src/main/java/com/hfad/mapapp/MainActivity.kt
+++ app/src/main/java/com/hfad/mapapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hfad.mapapp
 
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,7 +9,6 @@ import com.yandex.mapkit.MapKitFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var startFragment: StartFragment
-    private val locationPermissionCode = 2
     private lateinit var transaction: FragmentTransaction
     private val MAPKIT_API_KEY: String = "fe35bed6-6bef-401c-a925-5d12055f65a5"
 
@@ -31,23 +29,7 @@ class MainActivity : AppCompatActivity() {
     private fun openFragment(fragment: Fragment) {
         transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.activity_main, fragment)
-        transaction.replace(R.id.activity_main, fragment)
         transaction.commit()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == locationPermissionCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-            } else {
-                //Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
 
